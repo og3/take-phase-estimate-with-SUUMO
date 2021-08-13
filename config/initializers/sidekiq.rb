5d732b2d34577fc341f825dbd1edff98c381e35e
og3 <>
2021-08-13 10:32:41 +0900

    herokuで動かす設定

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379', namespace: 'suumo_sidekiq' }
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379'), namespace: 'suumo_sidekiq' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379', namespace: 'suumo_sidekiq' }
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379'), namespace: 'suumo_sidekiq' }
 end
