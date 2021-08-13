87ce1b4fcb3af0c53168d5c4e166ee2b20998ac8
og3 <>
2021-08-13 11:03:28 +0900

    本番環境にメール認証機能を設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,21 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # 認証メールの送信設定
+  # default url
+  config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 }
+  # mail setting
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => ENV['MAILER_SENDER_GMAIL_ADRESS'],
+    :password => ENV['MAILER_SENDER_GMAIL_ADRESS_PASSWORD'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
