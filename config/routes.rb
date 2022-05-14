d43089464d9aab2b00ea5a44a3606c4b88336bc8
og3 <>
2022-05-14 10:49:31 +0900

    証明書更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # ssl証明書発行用
+  get ".well-known/acme-challenge/:id" => "ssl#letsencrypt"
+
   root "phase_estimate#input_url"
   get "/input_url" => "phase_estimate#input_url"
   post "/check_url" => "phase_estimate#check_url"
