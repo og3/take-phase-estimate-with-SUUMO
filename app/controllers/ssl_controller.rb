695cd086f2c9c9416f7e3dbf30f9df3cb501cb71
og3 <>
2022-05-14 11:21:57 +0900

    missing template回避

--- app/controllers/ssl_controller.rb
+++ app/controllers/ssl_controller.rb
@@ -1,7 +1,7 @@
 class SslController < ApplicationController
   def letsencrypt
     if params[:id] == ENV["LETSENCRYPT_REQUEST"]
-    render text: ENV["LETSENCRYPT_RESPONSE"]
+      render plain: ENV["LETSENCRYPT_RESPONSE"]
     end
   end
 end
